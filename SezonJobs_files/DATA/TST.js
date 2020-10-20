7c0143a8c5f6d800ab4a94b1dce75059effdcf44
1_KingEr <53094736+1kinger@users.noreply.github.com>
2020-10-20 14:20:17 +0100

    Initial commit

---
+++ SezonJobs_files/DATA/TST.js
@@ -0,0 +1,9 @@
+
+Drzavi={
+	0:3,
+	1:["Germany",["Baden-Württemberg",  "Bayern", "Berlin", "Brandenburg", "Bremen", "Hamburg",  "Hessen", "Niedersachsen", "Mecklenburg-Vorpommern", "Nordrhein-Westfalen", "Rheinland-Pfalz", "Saarland",  "Sachsen", "Sachsen-Anhalt", "Schleswig-Holstein", "Thuringia"]],
+	2:["USA",["washington","los angeles"]],
+	3:["RUSIA",["moskva","putingrad"]]
+}
+		
+		
